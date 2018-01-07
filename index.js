17838e9c2aa7dfbd8b7e0badc4aa0d94902dfe53
binguanghe <binguanghe@gmail.com>
2018-01-07 15:23:37 +0800

    封装Spider函数

--- index.js
+++ index.js
@@ -6,41 +6,24 @@ var cheerio = require('cheerio');
 //数据库spider集合news
 var news = require('./db/database.js').news;
 
-var options = {
-	host: "www.huxiu.com",
-	path: "",
-	headers: {
-	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36'
-	}
-};
-
 //请求函数
-
-function Spider(){
-	https.get(options, function(res){
+function Spider(ruler){
+	https.get(ruler.getOptions, function(res){
 		var html = '';
-		var titles = [];
 		res.setEncoding('utf-8');
 		//监听data事件
 		res.on('data', function(chunk){
 			html += chunk;
 		});
-
 		//监听end事件,响应接收完成后处理
 		res.on('end', function(){  
 			var $ = cheerio.load(html);
-			var num = 0;
-			var website = "虎嗅网";
-			$(".mob-ctt").each(function(index, item){
-				//获取标题
-				var title = $(this).children().children().first().text();
-				//获取文章链接
-				var url = "https://www.huxiu.com" + $(this).children().children().first().attr("href");
+			$(ruler.className).each(function(index, item){
 				var NewsEntity = new news({
-				    num: num+=1,
-				    website: website,
-				    title: title,
-				    url: url
+				    num: ruler.num+=1,
+				    website: ruler.website,
+				    title: eval(ruler.title),
+				    url: eval(ruler.url)
 				});
 
 				//保存数据到数据库
@@ -51,33 +34,24 @@ function Spider(){
 				        console.log(doc)
 				    }
 				});
-				//处理要插入数据库的数据
-				/*var insertData = function(db){
-					num += 1;
-					var collection = db.collection('news');
-					collection.insert({num,title,url}, function(err, result){
-						if(err){
-							console.log("ERR: "+err);
-						}else{
-							console.log(result);
-
-						}
-					})
-				};
-
-				//连接数据库并插入
-				MongoClient.connect(DB_CONN_STR, function(err,db){
-					insertData(db,function(){
-						db.close();
-					});
-				})
-			});*/
-		
 		  });	
 		});
 	});
-
-
 }
 
-Spider();
+//定义规则
+var ruler = {
+	getOptions: {
+		host: "www.huxiu.com",
+		path: "",
+		headers: {
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36'
+	    }
+	},
+	className: ".mob-ctt",
+	num: 0,
+	website: "虎嗅网",
+	title: "$(this).children().children().first().text()",
+	url: '"https://www.huxiu.com" + $(this).children().children().first().attr("href")',
+}
+Spider(ruler)
